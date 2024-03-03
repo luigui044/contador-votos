fc976a54cc4414666d8cec87e87b3186c96fa743
luigui044 <56895934+luigui044@users.noreply.github.com>
2024-03-03 16:11:23 -0600

    corrigiendo responsive

--- resources/views/dashboard-actas.blade.php
+++ resources/views/dashboard-actas.blade.php
@@ -1,91 +1,83 @@
 @extends('layouts.master')
 @section('recarga')
-<meta http-equiv="refresh" content="30">
-    
+    <meta http-equiv="refresh" content="30">
 @endsection
 
 @section('cssmas')
-    
-<style>
-    body{
-        background-color: #1d2d44;
+    <style>
+        body {
+            background-color: #1d2d44;
 
-    }
-    #chartdiv{
-      width: 100%;
-      height: 600px;
-      padding-left: 20px;
-    }
-    #pastel{
-        width: 100%;
-      height: 450px;
-    }
-    #podometro,#podometro2{
-        width: 100%;
-      height: 300px;
-    }
-    #barra{
-        width: 100%;
-      height: 300px;
-    }
-    
+        }
+
+        #chartdiv {
+            width: 100%;
+            height: 600px;
+            padding-left: 20px;
+        }
+
+        #pastel {
+            width: 100%;
+            height: 450px;
+        }
+
+        #podometro,
+        #podometro2 {
+            width: 100%;
+            height: 300px;
+        }
+
+        #barra {
+            width: 100%;
+            height: 300px;
+        }
     </style>
-    
 @endsection
 
 @include('footer')
 @section('contenedor')
-
     <div class="row">
 
-        <div class="col-md-4" >
-            <div id="chartdiv" ></div>
+        <div class="col-12 col-lg-4">
+            <div id="chartdiv"></div>
 
         </div>
-    <div class="col-md-3">
-        <div class="row">
-            <div class="col-md-12">
-                <div id="podometro"></div>
+        <div class="col-12 col-lg-3">
+            <div class="row">
+                <div class="col-12 col-lg-12">
+                    <div id="podometro"></div>
+
+                </div>
+                <div class="col-12 col-lg-12">
+                    <div id="podometro2"></div>
+
+                </div>
 
             </div>
-            <div class="col-md-12">
-                <div id="podometro2"></div>
+        </div>
+        <div class="col-12 col-lg-5">
+            <div class="row">
+                <div class="col-12 col-lg-12">
+                    <div id="pastel"></div>
+                </div>
 
             </div>
-            
         </div>
-    </div>
-    <div class="col-md-5">
-        <div class="row">
-            <div class="col-md-12">
-                <div id="pastel"></div>
-            </div>
-            
-        </div>
-    </div>
-    <div class="col-md-6">
+        <div class="col-12 col-lg-6">
             <div id="barra"></div>
+        </div>
     </div>
-    </div>
-
-
-        
-
-
-
-
-
 @endsection
 
 
 
 @section('jsmas')
-<script src="{{asset('amcharts/core.js')}}"></script>
-<script src="{{asset('amcharts/charts.js')}}"></script>
-<script src="{{asset('amcharts/maps.js')}}"></script>
-<script src="//www.amcharts.com/lib/4/themes/dark.js"></script>
+    <script src="{{ asset('amcharts/core.js') }}"></script>
+    <script src="{{ asset('amcharts/charts.js') }}"></script>
+    <script src="{{ asset('amcharts/maps.js') }}"></script>
+    <script src="//www.amcharts.com/lib/4/themes/dark.js"></script>
 
-<script src="{{asset('amcharts/themes/animated.js')}}"></script>
+    <script src="{{ asset('amcharts/themes/animated.js') }}"></script>
 
-<script src="{{asset('assets/js/graficos.js')}}"></script>
+    <script src="{{ asset('assets/js/graficos.js') }}"></script>
 @endsection
